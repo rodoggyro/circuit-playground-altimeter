7ccb776bfcd7893c39f7fe4dd84363a4817cd053
rodoggyro <rowans@hotmail.com>
2019-02-05 08:26:21 -0500

    adds working code for altimeter

--- code.py
+++ code.py
@@ -7,6 +7,8 @@ import time
 import board
 import microcontroller
 import digitalio
+import busio
+import adafruit_mpl3115a2
 
 def get_temperature():
     counter = 1
@@ -23,9 +25,71 @@ def get_temperature():
                 print('Both buttons are pressed.')
                 break
 
+# ######################################################################################################
+# Checks if Circuit Playground is in read/write mode
+# ######################################################
+def cpxWriteMode():
+    switch = cpx.switch
+    if switch == False:
+        # CPx can write to the storage
+        cpx.red_led = True
+        return True
+    else:
+        # Computer can write to the storage
+        cpx.red_led = False
+        return False
+# ######################################################
+
+# ######################################################################################################
+# Gets altitude from MPL3115A2
+# Links:
+#    Tutorial: https://github.com/adafruit/Adafruit_CircuitPython_MPL3115A2/blob/master/examples/simpletest.py
+#    Weather: https://forecast.weather.gov/MapClick.php?lat=40.8764&lon=-73.6029#.XFj_BVxKhaQ
+# ######################################################
+def get_altitude():
+    PRESSURE_SEA_LEVEL_PASCALS = 101626
+
+    # Initialize the I2C bus.
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    # Initialize the MPL3115A2.
+    sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
+
+    # Set this to a value in pascals:
+    sensor.sealevel_pressure = PRESSURE_SEA_LEVEL_PASCALS
+
+    print('*** BEGIN MEASUREMENTS ***')
+
+    if cpxWriteMode():
+        logger = open("/log.txt", "a")
+
+    while True:
+        altitude = sensor.altitude
+        print('Altitude: {0:0.3f} meters'.format(altitude))
+
+        if cpxWriteMode():
+            logger.write('Altitude: {0:0.3f} meters\r\n'.format(altitude))
+            logger.flush()
+
+        time.sleep(0.2)
+
+        if cpx.button_a == True and cpx.button_b == True:
+            print('*** END MEASUREMENTS ***')
+
+            if cpxWriteMode():
+                logger.write('*** END MEASUREMENTS ***')
+                logger.flush()
+                logger.close()
+
+            break
+
+# ######################################################
+
+
 #true = read and write
 
 # if cpx.switch == False:
 #     cpx.red_led = True
 
-get_temperature()
+#get_temperature()
+get_altitude()
